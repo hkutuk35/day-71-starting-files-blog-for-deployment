e5f2a9cbc9ec38cc29e9907c4bb75cec32b14d87
haydar_kutuk <haydar_kutuk@yahoo.com>
2025-06-30 22:55:03 -0700

    Use environment variables and create Procfile

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 # Optional: add contact me email functionality (Day 60)
 # import smtplib
 
+import os
 
 '''
 Make sure the required packages are installed: 
@@ -29,7 +30,8 @@ This will install the packages from the requirements.txt for this project.
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+#app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -56,7 +58,9 @@ gravatar = Gravatar(app,
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
+
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -298,5 +302,7 @@ def contact():
 #         connection.sendmail(MAIL_ADDRESS, MAIL_APP_PW, email_message)
 
 
+# if __name__ == "__main__":
+#     app.run(debug=True, port=5001)
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=False)
